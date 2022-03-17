a1e4f0da1e0306f9bf502fe89eff7775ecb42bde
Alessandro Sosso <alesosso@gmail.com>
2022-03-17 15:35:58 +0100

    Added new_report request in eduroam_reporter service

--- services/eduroam_reporter/eduroam_reporter.py
+++ services/eduroam_reporter/eduroam_reporter.py
@@ -1,5 +1,6 @@
 from modules.base_service import BaseService
 from . import var
+from datetime import datetime
 import os
 import logging
 
@@ -16,3 +17,9 @@ class EduroamReporter(BaseService):
 
     def _request_get_default_reports(self):
         return self.default_reports
+
+    def _request_new_report(self, user_id, student_infos, report, place, note):
+        time = datetime.now().strftime(var.DATETIME_FORMAT)
+        header = f"name={student_infos['name']}\nsurname={student_infos['surname']}\nuser_id={user_id}\ntime={time}"
+        with open(os.path.join(var.REPORTS_DIR, f"{user_id} - {time}.errp"), 'w', encoding='utf-8') as f:
+            f.write(f"{header}\n\nPLACE:\t {place}\nREPORT:\t {report}\nNOTE:\t {note}")
