3b35dab5a54305b47f6dfdd569bf0d652d225ecf
Alessandro Sosso <alesosso@gmail.com>
2022-03-18 16:26:49 +0100

    Removed unused variables in eduroam_reporter

--- services/eduroam_reporter/var.py
+++ services/eduroam_reporter/var.py
@@ -9,10 +9,3 @@ REPORTS_DIR = os.path.join(DATA_DIR, 'reports')
 # Files
 DATETIME_FORMAT = '%Y-%m-%d %H:%M:%S'
 DEFAULT_REPORTS_FILE = os.path.join(DATA_DIR, 'default_reports.erdr')
-
-# Reporter
-UPDATE_SECONDS_INTERVAL = 10
-
-# Reports
-REPORTS_LIST = ("Il wi-fi non si connette",
-                "Nessuna connessione ad Internet, ma al wi-fi sì")
