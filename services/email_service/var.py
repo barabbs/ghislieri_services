7ef064695744af47e6a0c9d7bc721fce8dfeef57
alessandro <alesosso@gmail.com>
2022-06-10 16:58:13 +0200

    Changed authentication to google in email_service with oauth2

--- services/email_service/var.py
+++ services/email_service/var.py
@@ -9,8 +9,11 @@ if DEBUG:
     DATA_DIR = os.path.join(os.getcwd(), 'data', SERVICE_NAME)
 ATTACHMENT_DIR = os.path.join(DATA_DIR, "attachments")
 
-# Files
-CREDENTIALS_FILE = os.path.join(DATA_DIR, 'email_credentials_file.escr')
+# Credentials
+GOOGLE_SCOPES = ["https://mail.google.com/"]
+GOOGLE_CREDENTIALS_FILE = os.path.join(DATA_DIR, 'google_credentials.json')
+GOOGLE_TOKEN_FILE = os.path.join(DATA_DIR, 'google_token.json')
+GOOGLE_CREDENTIALS_NOTIFICATION_DATA = {"groups": ('master',), "n_type": "google_credentials", "msg_code": "notifications.admin.google_credentials_login", "notify": True, "enabled_time": {"hours": 12}}
 
 # Server
 SMTP_HOST, IMAP_HOST = "smtp.gmail.com", "imap.gmail.com"
