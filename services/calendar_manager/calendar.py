eff8611451c7af206c37e137d54136853657418f
alessandro <alesosso@gmail.com>
2022-12-08 21:35:34 +0100

    Debug

--- services/calendar_manager/calendar.py
+++ services/calendar_manager/calendar.py
@@ -54,6 +54,9 @@ def get_event_daily_recap(event, day):
                "hidden_url": f'<a href="{event.url}"> </a>',
                "url": event.url,
                "uid": event.uid}
+    cats = dict()
+    for c in (v for v in var.CATEGORIES_BY_CLASS.keys() if v.split(".")[0] == event.classification):
+        cats.update(var.CATEGORIES_BY_CLASS[c])
     for key, sym in var.CATEGORIES_BY_CLASS[event.classification].items():
         if key in event.categories:
             ev_dict['symbol'] = sym[1]
